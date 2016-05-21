187fbac379b52ba7b4c26801f02d0dc356b5310b
aikin <ljlu@thoughtworks.com>
2016-05-21 17:46:18 +0800

    aikin: add FindCostMoreThanThreePerUnitProductsLinq

--- RestrictionOperators/Program.cs
+++ RestrictionOperators/Program.cs
@@ -15,7 +15,37 @@ namespace RestrictionOperators
             restrictionOperators.FindNumbersLessThanFiveLinq(DataProvider.GivenNumbers());
             restrictionOperators.FindNumbersLessThanFiveLambda(DataProvider.GivenNumbers());
 
-       
+
+            /*
+                This sample uses the where clause to find all products that are out of stock.
+            */
+            restrictionOperators.FindOutOfStockProductsLinq(DataProvider.GivenProducts());
+            restrictionOperators.FindOutOfStockProductsLambda(DataProvider.GivenProducts());
+
+
+            /*
+                This sample uses the where clause to find all products that are in stock and cost more than 3.00 per unit.
+            */
+            restrictionOperators.FindCostMoreThanThreePerUnitProductsLinq(DataProvider.GivenProducts());
+            restrictionOperators.FindCostMoreThanThreePerUnitProductsLambda(DataProvider.GivenProducts());
+
+
+            /*
+                This sample uses the where clause to find all products that are out of stock.
+            */
+
+
+
+            /*
+                This sample uses the where clause to find all products that are out of stock.
+            */
+
+
+
+            /*
+                This sample uses the where clause to find all products that are out of stock.
+            */
+
         }
     }
 }
--- RestrictionOperators/RestrictionOperators.cs
+++ RestrictionOperators/RestrictionOperators.cs
@@ -33,5 +33,56 @@ namespace RestrictionOperators
             }
         }
 
+        public void FindOutOfStockProductsLinq(List<Product> products)
+        {
+            var soldOutProducts =
+                from prod in products
+                where prod.UnitsInStock == 0
+                select prod;
+
+            Debug.WriteLine("Sold out products:");
+            foreach (var product in soldOutProducts)
+            {
+                Debug.WriteLine("{0} is sold out!", product.Name);
+            }
+        }
+
+        public void FindOutOfStockProductsLambda(List<Product> products)
+        {
+            var soldOutProducts = products.Where(prod => prod.UnitsInStock == 0);
+
+            Debug.WriteLine("Sold out products:");
+            foreach (var product in soldOutProducts)
+            {
+                Debug.WriteLine("{0} is sold out!", product.Name);
+            }
+        }
+
+        public void FindCostMoreThanThreePerUnitProductsLinq(List<Product> products)
+        {
+            var expensiveInStockProducts =
+                   from prod in products
+                   where prod.UnitsInStock > 0 && prod.UnitPrice > 3.00M
+                   select prod;
+
+            Debug.WriteLine("In-stock products that cost more than 3.00:");
+            foreach (var product in expensiveInStockProducts)
+            {
+                Debug.WriteLine("{0} is in stock and costs more than 3.00.", product.Name);
+            }
+
+        }
+
+        public void FindCostMoreThanThreePerUnitProductsLambda(List<Product> products)
+        {
+
+            var expensiveInStockProducts = products.Where(prod => prod.UnitsInStock > 0 && prod.UnitPrice > 3.00M);
+
+            Debug.WriteLine("In-stock products that cost more than 3.00:");
+            foreach (var product in expensiveInStockProducts)
+            {
+                Debug.WriteLine("{0} is in stock and costs more than 3.00.", product.Name);
+            }
+        }
     }
 }
