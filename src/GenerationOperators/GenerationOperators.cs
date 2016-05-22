6a30dcf8a17904622c64447dbe64c9a884405cd4
aikin <me@ulaijn.com>
2016-05-22 18:16:52 +0800

    aikin: add generation operator

---
+++ src/GenerationOperators/GenerationOperators.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+using System.Linq;
+
+namespace GenerationOperators
+{
+    public class GenerationOperators
+    {
+        public void GenerateNumbersFrom100to149()
+        {
+            var numbers = Enumerable.Range(100, 50).Select(num => new { Number = num, OddEven = num % 2 == 1 ? "odd" : "even" });
+
+            foreach (var n in numbers)
+            {
+                Debug.WriteLine("The number {0} is {1}.", n.Number, n.OddEven);
+            }
+        }
+
+        public void GenerateRepeatNumber7()
+        {
+            var numbers = Enumerable.Repeat(7, 10);
+
+            foreach (var n in numbers)
+            {
+                Debug.WriteLine(n);
+            }
+        }
+    }
+}
---
+++ src/GenerationOperators/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenerationOperators
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            var generationOperators = new GenerationOperators();
+
+            /*
+                This sample uses Range to generate a sequence of numbers from 100 to 149 that 
+                is used to find which numbers in that range are odd and even.
+            */
+            generationOperators.GenerateNumbersFrom100to149();
+
+
+            /*
+                This sample uses Repeat to generate a sequence that contains the number 7 ten times.
+            */
+            generationOperators.GenerateRepeatNumber7();
+
+
+        }
+    }
+}
