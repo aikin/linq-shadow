d00384575464be76266036ba477336b3254ba27f
aikin <me@ulaijn.com>
2016-05-22 18:55:38 +0800

    aikin: add Quantifiters

---
+++ src/Quantifiers/Program.cs
@@ -0,0 +1,30 @@
+﻿using Linq.Shadow.Common;
+
+namespace Quantifiers
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var quantifiers = new Quantifiers();
+
+            /*
+                This sample uses Any to determine if any of the words in the array contain the substring 'ei'.
+            */
+            quantifiers.DetermineWordsContainEI(DataProvider.GivenWords());
+
+            /*
+                This sample uses Any to return a grouped a list of products only for categories that 
+                have at least one product that is out of stock.
+            */
+            quantifiers.GroupProductsHaveAtLeastOneProduct(DataProvider.GivenProducts());
+
+
+            /*
+                This sample uses All to return a grouped a list of products only for categories that 
+                have all of their products in stock.
+            */
+            quantifiers.GroupProductsAllInStock(DataProvider.GivenProducts());
+        }
+    }
+}
---
+++ src/Quantifiers/Quantifiers.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using Linq.Shadow.Common;
+
+namespace Quantifiers
+{
+    public class Quantifiers
+    {
+        public void DetermineWordsContainEI(string[] words)
+        {
+            var iAfterE = words.Any(w => w.Contains("ei"));
+
+            Debug.WriteLine("There is a word in the list that contains 'ei': {0}", iAfterE);
+        }
+
+        public void GroupProductsHaveAtLeastOneProduct(List<Product> products)
+        {
+            var productGroups = products.GroupBy(prod => prod.Category)
+                .Where(prodGroup => prodGroup.Any(p => p.UnitsInStock == 0))
+                .Select(prodGroup => new
+                {
+                    Category = prodGroup.Key,
+                    Products = prodGroup
+                });
+
+
+            Debug.WriteLine("Grouped Products: ");
+            foreach (var g in productGroups)
+            {
+                Debug.WriteLine("Category: {0}", g.Category);
+                foreach (var w in g.Products)
+                {
+                    Debug.WriteLine("\t" + w.Name);
+                }
+            }
+        }
+
+        public void GroupProductsAllInStock(List<Product> products)
+        {
+            var productGroups = products.GroupBy(prod => prod.Category)
+                .Where(prodGroup => prodGroup.All(p => p.UnitsInStock > 0))
+                .Select(prodGroup => new
+                {
+                    Category = prodGroup.Key,
+                    Products = prodGroup
+                });
+
+            Debug.WriteLine("Grouped Products: ");
+            foreach (var g in productGroups)
+            {
+                Debug.WriteLine("Category: {0}", g.Category);
+                foreach (var w in g.Products)
+                {
+                    Debug.WriteLine("\t" + w.Name);
+                }
+            }
+        }
+    }
+}
