832955cfc7d2a4e05460f5d4ccca42016083d79e
aikin <me@ulaijn.com>
2016-05-22 17:45:09 +0800

    aikin: element operators

---
+++ src/ElementOperators/ElementOperators.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using Linq.Shadow.Common;
+
+namespace ElementOperators
+{
+    public class ElementOperators
+    {
+        public void FirstElementStartsWithOLetter(string[] digits)
+        {
+            var startsWithO = digits.First(s => s[0] == 'o');
+
+            Debug.WriteLine("A string starting with 'o': {0}", startsWithO);
+        }
+
+        public void FirstDefaultWhenNoElements()
+        {
+            int[] numbers = { };
+
+            var firstNumOrDefault = numbers.FirstOrDefault();
+
+            Debug.WriteLine(firstNumOrDefault);
+        }
+
+        public void FisrtDefaultProductWhichProductIdIs789(List<Product> products)
+        {
+            var product789 = products.FirstOrDefault(p => p.Id == 789);
+
+            Debug.WriteLine("Product 789 exists: {0}", product789 != null);
+        }
+
+        public void RetrieveSecondNumberGreaterThan5(int[] numbers)
+        {
+            var fourthLowNum = numbers.Where(num => num > 5).ElementAt(1);
+
+            Debug.WriteLine("Second number > 5: {0}", fourthLowNum);
+        }
+    }
+}
---
+++ src/ElementOperators/Program.cs
@@ -0,0 +1,42 @@
+﻿using Linq.Shadow.Common;
+
+namespace ElementOperators
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var elementOperators = new ElementOperators();
+
+
+            /*
+                This sample uses First to find the first element in the array that starts with 'o'.
+            */
+            elementOperators.FirstElementStartsWithOLetter(DataProvider.GivenDigits());
+
+
+            /*
+                This sample uses FirstOrDefault to try to return the first element of the sequence, unless there are 
+                no elements, in which case the default value for that type is returned. FirstOrDefault is useful 
+                for creating outer joins.
+            */
+            elementOperators.FirstDefaultWhenNoElements();
+
+
+
+            /*
+                This sample uses FirstOrDefault to return the first product whose ProductID is 789 as a 
+                single Product object, unless there is no match, in which case null is returned.
+            */
+            elementOperators.FisrtDefaultProductWhichProductIdIs789(DataProvider.GivenProducts());
+
+
+
+            /*
+                This sample uses ElementAt to retrieve the second number greater than 5 from an array.
+            */
+            elementOperators.RetrieveSecondNumberGreaterThan5(DataProvider.GivenNumbers());
+
+        }
+    }
+}
