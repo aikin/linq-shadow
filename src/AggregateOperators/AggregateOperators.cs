4dfe82f3610fb6c6f709c30e454f22f7bafb342c
aikin <me@ulaijn.com>
2016-05-23 00:07:58 +0800

    aikin: add aggregate operator

---
+++ src/AggregateOperators/AggregateOperators.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using Linq.Shadow.Common;
+
+namespace AggregateOperators
+{
+    public class AggregateOperators
+    {
+        public void CountOddNumbers(int[] numbers)
+        {
+            var oddNumbers = numbers.Count(n => n % 2 == 1);
+
+            Debug.WriteLine("There are {0} odd numbers in the list.", oddNumbers);
+        }
+
+        public void CountCustomerOrders(List<Customer> customers, List<Order> orders)
+        {
+            var orderCounts = customers.Select(cust => new {
+                CustomerId = cust.Id,
+                OrderCount = orders.Count(order => cust.Id == order.CustomerId)
+            });
+
+
+            Debug.WriteLine("Customer Orders Count: ");
+            foreach (var orderCount in orderCounts)
+            {
+                Debug.WriteLine($"CustomerId: {orderCount.CustomerId}    OrderCount: {orderCount.OrderCount}");
+            }
+        }
+
+        public void CountProductsInEachCategory(List<Product> products)
+        {
+
+            var categoryCounts = products
+                .GroupBy(prod => prod.Category)
+                .Select(prodGroup => new
+                {
+                    Category = prodGroup.Key,
+                    ProductCount = prodGroup.Count()
+                });
+
+            Debug.WriteLine("Category Products Count: ");
+            foreach (var categoryCount in categoryCounts)
+            {
+                Debug.WriteLine($"Category: {categoryCount.Category}    ProductCount: {categoryCount.ProductCount}");
+            }
+        }
+    }
+}
---
+++ src/AggregateOperators/Program.cs
@@ -0,0 +1,34 @@
+﻿using Linq.Shadow.Common;
+
+namespace AggregateOperators
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var aggregateOperators = new AggregateOperators();
+
+            /*
+                This sample uses Count to get the number of odd ints in the array.
+            */
+            aggregateOperators.CountOddNumbers(DataProvider.GivenNumbers());
+
+
+            /*
+                This sample uses Count to return a list of customers and how many orders each has.
+            */
+            aggregateOperators.CountCustomerOrders(DataProvider.GivenCustomers(), DataProvider.GivenOrders());
+
+
+
+            /*
+                This sample uses Count to return a list of categories and how many products each has.
+            */
+            aggregateOperators.CountProductsInEachCategory(DataProvider.GivenProducts());
+
+
+
+
+        }
+    }
+}
