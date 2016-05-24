c09ab266b48bb127c28b413717b8b6151d8f9485
aikin <me@ulaijn.com>
2016-05-24 22:31:13 +0800

    aikin: add  MiscellaneousOperators

---
+++ src/MiscellaneousOperators/MiscellaneousOperators.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+using System.Linq;
+
+namespace MiscellaneousOperators
+{
+    public class MiscellaneousOperators
+    {
+        public void ConcatArrayOneAfterTheOther(int[] numbersA, int[] numbersB)
+        {
+            var allNumbers = numbersA.Concat(numbersB);
+
+            Debug.WriteLine("All numbers from both arrays:");
+            foreach (var n in allNumbers)
+            {
+                Debug.WriteLine(n);
+            }
+        }
+
+        public void CompareTwoSequencesAllElementsInSameOrder(string[] wordsA, string[] wordsB)
+        {
+
+            var match = wordsA.SequenceEqual(wordsB);
+
+            Debug.WriteLine($"The sequences match: {match}");
+        }
+    }
+}
---
+++ src/MiscellaneousOperators/Program.cs
@@ -0,0 +1,26 @@
+﻿using Linq.Shadow.Common;
+
+namespace MiscellaneousOperators
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            var miscellaneousOperators = new MiscellaneousOperators();
+
+
+            /*
+                This sample uses Concat to create one sequence that contains each array's values, one after the other.
+            */
+            miscellaneousOperators.ConcatArrayOneAfterTheOther(DataProvider.GivenNumbersA(), DataProvider.GivenNumbersB());
+
+
+            /*
+                This sample uses SequenceEquals to see if two sequences match on all elements in the same order.
+            */
+            miscellaneousOperators.CompareTwoSequencesAllElementsInSameOrder(DataProvider.GivenWords(), DataProvider.GivenWords());
+
+        }
+    }
+}
