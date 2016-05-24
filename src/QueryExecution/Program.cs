ff0cdd479b2d16f0a0ba0cb0e8d8974e3fb2c076
aikin <me@ulaijn.com>
2016-05-24 23:07:26 +0800

    aikin: add query execution

---
+++ src/QueryExecution/Program.cs
@@ -0,0 +1,29 @@
+﻿using Linq.Shadow.Common;
+
+namespace QueryExecution
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var queryExecution = new QueryExecution();
+
+            /*
+                The following sample shows how query execution is deferred until the query is enumerated at a foreach statement.
+            */
+            queryExecution.DeferredExecution(DataProvider.GivenNumbers());
+
+
+            /*
+               The following sample shows how, because of deferred execution, queries can be used again after data changes and will then operate on the new data.
+           */
+            queryExecution.QueryReuse(DataProvider.GivenNumbers());            
+            
+            
+            /*
+                The following sample shows how queries can be executed immediately, and their results stored in memory, with methods such as ToList.
+            */
+            queryExecution.ImmediateExecution(DataProvider.GivenNumbers());
+        }
+    }
+}
---
+++ src/QueryExecution/QueryExecution.cs
@@ -0,0 +1,77 @@
+﻿using System.Diagnostics;
+using System.Linq;
+
+namespace QueryExecution
+{
+    public class QueryExecution
+    {
+        public void ImmediateExecution(int[] numbers)
+        {
+            // Methods like ToList(), Max(), and Count() cause the query to be
+            // executed immediately.            
+            var i = 0;
+            var immediateQuery = numbers.Select(num => ++i).ToList();
+
+            Debug.WriteLine("The current value of i is {0}", i); //i has been incremented
+
+            foreach (var item in immediateQuery)
+            {
+                Debug.WriteLine("v = {0}, i = {1}", item, i);
+            }
+        }
+
+        public void DeferredExecution(int[] numbers)
+        {
+            var i = 0;
+            var simpleQuery = numbers.Select(num => ++i);
+
+            // The local variable 'i' is not incremented
+            // until the query is executed in the foreach loop.
+            Debug.WriteLine("The current value of i is {0}", i); //i is still zero
+
+            foreach (var item in simpleQuery)
+            {
+                Debug.WriteLine("v = {0}, i = {1}", item, i); // now i is incremented          
+            }
+        }
+
+        public void QueryReuse(int[] numbers)
+        {
+            // Deferred execution lets us define a query once
+            // and then reuse it later in various ways.
+            var lowNumbers = numbers.Where(num => num <= 3);
+
+            Debug.WriteLine("First run numbers <= 3:");
+            foreach (var n in lowNumbers)
+            {
+                Debug.WriteLine(n);
+            }
+
+            // Query the original query.
+            var lowEvenNumbers = lowNumbers.Where(p => p % 2 == 0);
+
+            Debug.WriteLine("Run lowEvenNumbers query:");
+            foreach (var n in lowEvenNumbers)
+            {
+                Debug.WriteLine(n);
+            }
+
+
+            // Modify the source data.
+            for (var i = 0; i < 10; i++)
+            {
+                numbers[i] = -numbers[i];
+            }
+
+            // During this second run, the same query object,
+            // lowNumbers, will be iterating over the new state
+            // of numbers[], producing different results:
+            Debug.WriteLine("Second run numbers <= 3:");
+            foreach (var n in lowNumbers)
+            {
+                Debug.WriteLine(n);
+            }
+
+        }
+    }
+}
