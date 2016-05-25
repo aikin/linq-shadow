29c76abfdc2e6fb704338cde7105123ad4caed4f
aikin <me@ulaijn.com>
2016-05-25 22:35:21 +0800

    aikin: add join

---
+++ src/JoinOperators/JoinOperators.cs
@@ -0,0 +1,148 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using Linq.Shadow.Common;
+
+namespace JoinOperators
+{
+    public class JoinOperators
+    {
+        public void InnerJoinTwoSequencesLambda(List<Customer> customers, List<Supplier> suppliers)
+        {
+            var customerSupplierJoin = suppliers
+                .Join(customers, sup => sup.Country, cust => cust.Country, (sup, cust) => new
+                {
+                    Country = sup.Country,
+                    SupplierName = sup.SupplierName,
+                    CustomerName = cust.CompanyName
+                });
+
+            Debug.WriteLine("Suppliers Join Cumtomers");
+            foreach (var item in customerSupplierJoin)
+            {
+                Debug.WriteLine("Country = {0}, Supplier = {1}, Customer = {2}", item.Country, item.SupplierName,
+                    item.CustomerName);
+            }
+        }
+
+        public void InnerJoinTwoSequencesLinq(List<Customer> customers, List<Supplier> suppliers)
+        {
+            var customerSupplierJoin =
+                from sup in suppliers
+                join cust in customers on sup.Country equals cust.Country
+                select new
+                {
+                    Country = sup.Country,
+                    SupplierName = sup.SupplierName,
+                    CustomerName = cust.CompanyName
+                };
+
+            Debug.WriteLine("Suppliers Join Cumtomers");
+            foreach (var item in customerSupplierJoin)
+            {
+                Debug.WriteLine("Country = {0}, Supplier = {1}, Customer = {2}", item.Country, item.SupplierName,
+                    item.CustomerName);
+            }
+        }
+
+        public void GroupJoinTwoSequencesLambda(List<Customer> customers, List<Supplier> suppliers)
+        {
+            var customerSupplierJoin = suppliers
+                .GroupJoin(customers, sup => sup.Country, cust => cust.Country, (sup, cust) => new
+                {
+                    Key = sup.Country,
+                    Customers = cust
+                });
+
+            Debug.WriteLine("Suppliers Group Join Cumtomers");
+            foreach (var item in customerSupplierJoin)
+            {
+                Debug.WriteLine(item.Key + ":");
+                foreach (var customer in item.Customers)
+                {
+                    Debug.WriteLine("   " + customer.CompanyName);
+                }
+            }
+        }
+
+        public void GroupJoinTwoSequencesLinq(List<Customer> customers, List<Supplier> suppliers)
+        {
+            var customerSupplierJoin =
+                from sup in suppliers
+                join cust in customers on sup.Country equals cust.Country into cs
+                select new
+                {
+                    Key = sup.Country,
+                    Customers = cs
+                };
+
+
+            Debug.WriteLine("Suppliers Group Join Cumtomers");
+            foreach (var item in customerSupplierJoin)
+            {
+                Debug.WriteLine(item.Key + ":");
+                foreach (var customer in item.Customers)
+                {
+                    Debug.WriteLine("   " + customer.CompanyName);
+                }
+            }
+        }
+
+        public void CrossJoinByCategoriesLambda(List<Product> products)
+        {
+            var categories = new[]
+            {
+                "Beverages",
+                "Condiments",
+                "Vegetables",
+                "Dairy Products",
+                "Seafood"
+            };
+
+            var productsByCategory = categories
+                .GroupJoin(products, category => category, product => product.Category,
+                    (category, prods) => new {category, prods})
+                .SelectMany(productsWithCategory => productsWithCategory.prods, (productsWithCategory, product) => new
+                {
+                    Category = productsWithCategory.category,
+                    ProductName = product.Name
+                });
+
+
+            Debug.WriteLine("Products By Category: ");
+            foreach (var product in productsByCategory)
+            {
+                Debug.WriteLine(product.ProductName + ": " + product.Category);
+            }
+        }
+
+        public void CrossJoinByCategoriesLinq(List<Product> products)
+        {
+            var categories = new[]
+            {
+                "Beverages",
+                "Condiments",
+                "Vegetables",
+                "Dairy Products",
+                "Seafood"
+            };
+
+            var productsByCategory =
+                from cat in categories
+                join prod in products on cat equals prod.Category into ps
+                from p in ps
+                select new
+                {
+                    Category = p.Category,
+                    ProductName = p.Name
+                };
+
+
+            Debug.WriteLine("Products By Category: ");
+            foreach (var product in productsByCategory)
+            {
+                Debug.WriteLine(product.ProductName + ": " + product.Category);
+            }
+        }
+    }
+}
---
+++ src/JoinOperators/Program.cs
@@ -0,0 +1,39 @@
+﻿using Linq.Shadow.Common;
+
+namespace JoinOperators
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            var joinOperators = new JoinOperators();
+
+            /*
+                This sample shows how to perform a simple inner equijoin of two sequences to produce 
+                a flat result set that consists of each element in suppliers that has a matching element in customers.
+            */
+            joinOperators.InnerJoinTwoSequencesLambda(DataProvider.GivenCustomers(), DataProvider.GivenSuppliers());
+            joinOperators.InnerJoinTwoSequencesLinq(DataProvider.GivenCustomers(), DataProvider.GivenSuppliers());
+
+
+            /*
+                A group join produces a hierarchical sequence. 
+                The following query is an inner join that produces a sequence of objects, 
+                each of which has a key and an inner sequence of all matching elements.
+            */
+            joinOperators.GroupJoinTwoSequencesLambda(DataProvider.GivenCustomers(), DataProvider.GivenSuppliers());
+            joinOperators.GroupJoinTwoSequencesLinq(DataProvider.GivenCustomers(), DataProvider.GivenSuppliers());
+
+
+            /*
+                The group join operator is more general than join, as this slightly more verbose 
+                version of the cross join sample shows.
+            */
+            joinOperators.CrossJoinByCategoriesLambda(DataProvider.GivenProducts());
+            joinOperators.GroupJoinTwoSequencesLinq(DataProvider.GivenCustomers(), DataProvider.GivenSuppliers());
+
+
+        }
+    }
+}
